afddaf2a4d2d4026307b7b7f3ef404bbf0778d65
Amr Wahidi <amrw5189@gmail.com>
2025-11-21 10:58:36 +0200

    Clean repo + apply C++ .gitignore

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_XNet_FILE C:/Users/Amr/Desktop/Study/DSA Project/XML-Social-Network-Analyzer/build/XNet.exe)
-set(__QT_DEPLOY_TARGET_XNet_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_XNet_RUNTIME_DLLS D:/QT/6.9.3/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.9.3/mingw_64/bin/Qt6Gui.dll;D:/QT/6.9.3/mingw_64/bin/Qt6Core.dll)
--- build/.qt/deploy_XNet_b924afa99b.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/Amr/Desktop/Study/DSA Project/XML-Social-Network-Analyzer/build/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/XNet-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/Amr/Desktop/Study/DSA Project/XML-Social-Network-Analyzer/build/XNet.exe"
-    GENERATE_QT_CONF
-)
