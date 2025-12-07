98d5a59fa234035bac01b7527e6b290047022fbf
Hamza Eldafrawy <hamza.eldefrawy4@gmail.com>
2025-12-08 00:22:26 +0200

    GUI Converter page done

---
+++ src/UI/pages/json_converter_page.h
@@ -0,0 +1,34 @@
+#ifndef JSONCONVERTERPAGE_H
+#define JSONCONVERTERPAGE_H
+
+#include <QWidget>
+#include <QString>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class JsonConverterPage; }
+QT_END_NAMESPACE
+
+class JsonConverterPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit JsonConverterPage(QWidget *parent = nullptr);
+    ~JsonConverterPage();
+
+private slots:
+    void onBrowseFile();
+    void onConvertToJSON();
+    void onDownload();
+    void onBackToOperations();
+
+private:
+    Ui::JsonConverterPage *ui;
+    QString currentFilePath;
+    QString inputXML;
+    QString outputJSON;
+    
+    void updateOutputVisibility();
+};
+
+#endif
